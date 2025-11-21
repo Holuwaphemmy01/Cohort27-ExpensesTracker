a9df745185db9ebb7ec9ed2dbf6d3d93656259a9
adedotun <erik.edwards.jon@gmail.com>
2025-11-21 06:07:24 -0800

    RemoveExpense is working

---
+++ ExpenseTracker/src/RemoveExpense.js
@@ -0,0 +1,15 @@
+function RemoveExpense(product,expenses){
+    if(expenses.length == 0){
+        return "EXPENSES IS EMPTY!"
+    }
+    for(let count = 0; count < expenses.length; count++){
+        if(expenses[count]["purpose"]== product){
+            expenses.splice(count,1)
+            return "EXPENSE SUCCESSFULLY REMOVED!"
+        } 
+    }
+    return "INVALID INPUT!"
+
+}
+
+module.exports = RemoveExpense
